b83899a6fbb79ad2d222537e1d2b5288ae10f180
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-12-20 01:54:53 +0100

    tests: Add xmldoc for CheckWinUsage attribute test

--- OpenTabletDriver.Tests/ConfigurationTest/AttributesTest.cs
+++ OpenTabletDriver.Tests/ConfigurationTest/AttributesTest.cs
@@ -54,6 +54,11 @@ namespace OpenTabletDriver.Tests.ConfigurationTest
             internal static bool CheckFeatureInitDelayMs(string attributeValue) =>
                 uint.TryParse(attributeValue, out uint val) && val is > 0 and <= 10000;
 
+            /// <summary>
+            /// Checks if <c>WinUsage</c> key value falls within the expected range
+            /// </summary>
+            /// <param name="attributeValue">The value of the <c>WinUsage</c> key</param>
+            /// <returns><c>true</c> if string is a zero-padded string uint value that is exactly 2 characters</returns>
             internal static bool CheckWinUsage(string attributeValue)
             {
                 for (int i = 0; i < 100; i++)
