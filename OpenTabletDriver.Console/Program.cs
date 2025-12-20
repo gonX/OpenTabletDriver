ab3fbebd3d94ca7f69ca83da3d6ae613200f506a
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-12-20 05:13:27 +0100

    CLI: Add `maptodisplayindex` command

--- OpenTabletDriver.Console/Program.cs
+++ OpenTabletDriver.Console/Program.cs
@@ -75,6 +75,7 @@ namespace OpenTabletDriver.Console
             CreateCommand<string[]>(EnableTools, "Enables the specified tools"),
             CreateCommand<string[]>(DisableTools, "Disables the specified tools"),
             CreateCommand<string, float, float, float, float>(SetDisplayArea, "Sets the display area"),
+            CreateCommand<string, uint>(MapToDisplayIndex, "Map tablet to display index"),
             CreateCommand<string, float, float, float, float, float>(SetTabletArea, "Sets the tablet area"),
             CreateCommand<string, float, float, float>(SetSensitivity, "Sets the relative sensitivity"),
             CreateCommand<string, string, float>(SetTipBinding, "Sets the current tip binding"),
